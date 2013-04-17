92fe5ac422df4329225762ff3c035a044ec26055
Gray Hound <admin@devilmaydie.name>
2013-04-17 13:34:44 +0400

    Deleting old plugin files.

--- uploadcare/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-function uploadcare_settings() {
-	include('uploadcare_settings.php');
-}
-
-function uploadcare_setings_actions() {
-	add_options_page('Uploadcare Setting', 'Uploadcare Settings', 'upload_files', 'uploadcare', 'uploadcare_settings');
-}
-add_action('admin_menu', 'uploadcare_setings_actions');
