e99ad89c89dc066b48b64f28d4c0452c31bae9f3
Stanislau Shvaika <yan.total.all@gmail.com>
2015-04-22 23:54:57 +0300

    Code style

--- inc/actions.php
+++ inc/actions.php
@@ -232,19 +232,12 @@ function uploadcare_display_thumbnail_column($col, $id) {
 }
 
 
-function uploadcare_settings() {
-    include('uploadcare_settings.php');
-}
-
-
 /*
  * Add Uploadcare settings page to admin
  */
-add_action( 'admin_menu', 'uploadcare_add_admin_menu' );
-add_action( 'admin_init', 'uploadcare_settings_init' );
-
-
+add_action('admin_menu', 'uploadcare_add_admin_menu');
+add_action('admin_init', 'uploadcare_settings_init');
 function uploadcare_add_admin_menu(  ) {
-    add_options_page( 'Uploadcare', 'Uploadcare', 'manage_options', 'uploadcare', 'uploadcare_options_page' );
+    add_options_page('Uploadcare', 'Uploadcare', 'manage_options', 'uploadcare', 'uploadcare_options_page');
 }
-include( plugin_dir_path( __FILE__ ) . 'uploadcare_settings.php');
+include(plugin_dir_path( __FILE__ ) . 'uploadcare_settings.php');
