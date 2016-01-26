6150ac2b112277c7e1a3388d7f8d0eee4df49c37
Dmitry Petrov <dimaninc@gmail.com>
2016-01-26 19:46:25 +0300

    welcome screen added settings page moved to a function settings added to welcome screen

--- uploadcare.php
+++ uploadcare.php
@@ -65,3 +65,67 @@ function uploadcare_uninstall() {
     $wpdb->query("DROP TABLE IF EXISTS $thetable");
     */
 }
+
+/**
+ * Welcome screen
+ */
+
+register_activation_hook(__FILE__, 'welcome_screen_activate');
+function welcome_screen_activate() {
+    set_transient('_welcome_screen_activation_redirect', true, 30);
+}
+
+add_action('admin_init', 'welcome_screen_do_activation_redirect');
+function welcome_screen_do_activation_redirect() {
+    // Bail if no activation redirect
+    if (!get_transient('_welcome_screen_activation_redirect')) {
+        return;
+    }
+
+    // Delete the redirect transient
+    delete_transient('_welcome_screen_activation_redirect');
+
+    // Bail if activating from network, or bulk
+    if (is_network_admin() || isset($_GET['activate-multi'])) {
+        return;
+    }
+
+    // Redirect to bbPress about page
+    wp_safe_redirect(add_query_arg(array('page' => 'welcome-screen-about'), admin_url('index.php')));
+}
+
+add_action('admin_menu', 'welcome_screen_pages');
+
+function welcome_screen_pages() {
+    add_dashboard_page(
+        'Welcome To Welcome Screen',
+        'Welcome To Welcome Screen',
+        'read',
+        'welcome-screen-about',
+        'welcome_screen_content'
+    );
+}
+
+function welcome_screen_content() {
+?>
+    <div class="wrap">
+        <h2>Uploadcare Plugin</h2>
+
+        <p>
+            Here is a text about this plugin.
+        </p>
+
+        <div id="icon-options-general" class="icon32"><br></div>
+        <h2>Let's configure it</h2>
+
+        <?php print_base_settings("/wp-admin/options-general.php?page=uploadcare"); ?>
+
+    </div>
+<?php
+}
+
+add_action('admin_head', 'welcome_screen_remove_menus');
+
+function welcome_screen_remove_menus() {
+    remove_submenu_page('index.php', 'welcome-screen-about');
+}
