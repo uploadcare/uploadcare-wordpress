9aeabf6e3a1157faf428e510b4c9e021e685f535
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2018-05-14 22:22:46 +0300

    Update uploadcare-php version to 2.2.1 and update plugin version to 2.6.1

--- uploadcare.php
+++ uploadcare.php
@@ -14,7 +14,7 @@ if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
     exit("Uploadcare plugin requires PHP version <b>5.3+</b>, you've got <b>" . PHP_VERSION . "</b>");
 }
 
-define('UPLOADCARE_PLUGIN_VERSION', '2.6.0');
+define('UPLOADCARE_PLUGIN_VERSION', '2.6.1');
 define('UPLOADCARE_WIDGET_VERSION', '3.x');
 define('UPLOADCARE_TAB_EFFECTS_VERSION', '1.x');
 
