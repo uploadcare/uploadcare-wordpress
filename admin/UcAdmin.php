134d628adf0ce3df8dcab6adc761b1169e8a9b95
Andrew Zhdanovskih <azh@crtweb.ru>
2020-11-14 11:17:57 +0500

    Fix image sizes in case of one size is null

--- admin/UcAdmin.php
+++ admin/UcAdmin.php
@@ -500,6 +500,16 @@ HTML;
             if (9999 === $arr[1]) {
                 $arr[1] = 2048;
             }
+            if (!isset($arr[0]) || (int) $arr[0] === 0) {
+                if (isset($arr[1])) {
+                    $arr[0] = $arr[1];
+                }
+            }
+            if (!isset($arr[1]) || (int) $arr[1] === 0) {
+                if (isset($arr[0])) {
+                    $arr[1] = $arr[0];
+                }
+            }
 
             return $arr;
         }
