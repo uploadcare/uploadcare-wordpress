9c4d72759196156e9e0f8b25bd275eb03de770bf
Andrew Zhdanovskih <azh@crtweb.ru>
2020-12-17 08:37:31 +0500

    Error toggle link as link
    # Conflicts:
    #	includes/uploadcare_settings.php

--- includes/uploadcare_settings.php
+++ includes/uploadcare_settings.php
@@ -95,7 +95,7 @@ if (isset($_POST['uc_sync_data']) && $_POST['uc_sync_data'] === 'sync') {
 <?php if($connectError !== null): ?>
     <div class="error">
         <p><strong><?= __('Can\'t connect to the Uploadcare account. Check your public & secret keys.')?></strong></p>
-        <p id="error-collapse-toggle" class="uc-toggle"><?= __('More information') ?></p>
+        <p id="error-collapse-toggle" class="uc-toggle" style="color: #0d66c2; text-decoration: underline"><?= __('More information') ?></p>
         <div data-toggle="error-collapse-toggle" class="uc-collapsed hide" style="margin-bottom: 1rem">
             <pre><small><?= $connectError?></small></pre>
         </div>
