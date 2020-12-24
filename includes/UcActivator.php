85eedb428a1c21a5857ed2a0017da8cc20111d3e
Roman Sedykh <rsedykh@gmail.com>
2020-12-24 17:36:21 +0300

    Update UcActivator.php

--- includes/UcActivator.php
+++ includes/UcActivator.php
@@ -5,10 +5,10 @@ class UcActivator
     public static function activate()
     {
         if (!\function_exists('curl_init')) {
-            exit("Uploadcare plugin requires <b>php-curl</b> to function");
+            exit("Uploadcare plugin requires <b>php-curl</b> to function.");
         }
         if (!\class_exists(\DOMDocument::class)) {
-            exit("Uploadcare plugin requires <b>DOMDocument</b> class. Install ext-dom for activate this.");
+            exit('Uploadcare plugin requires <a href="https://www.php.net/manual/en/class.domdocument.php" target="_blank">DOMDocument</a> class. <a href="https://www.php.net/manual/en/dom.setup.php" target="_blank">Install ext-dom</a> for your PHP to function.');
         }
     }
 }
