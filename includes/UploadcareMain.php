92dfc852b8da07e716dc4fec25ef241024431835
Andrew Zhdanovskih <andrew72ru@gmail.com>
2021-03-01 18:35:00 +0500

    Remove not-used actions

--- includes/UploadcareMain.php
+++ includes/UploadcareMain.php
@@ -81,8 +81,8 @@ class UploadcareMain
 
         $this->loader->add_action('admin_head', $plugin_admin, 'loadAdminCss');
 //        $this->loader->add_action('admin_bar_menu', $this, 'adminBar', 100, 1);
-        $this->loader->add_action('plugins_loaded', $this, 'runUploadTask');
-        $this->loader->add_action('plugins_loaded', $this, 'runDownloadTask');
+//        $this->loader->add_action('plugins_loaded', $this, 'runUploadTask');
+//        $this->loader->add_action('plugins_loaded', $this, 'runDownloadTask');
         $this->loader->add_action('init', $plugin_admin, 'uploadcare_plugin_init');
         $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'add_uploadcare_js_to_admin');
         $this->loader->add_action('wp_ajax_uploadcare_handle', $plugin_admin, 'uploadcare_handle');
